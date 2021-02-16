b1de159ec84f0783a9b994d0ddd56156095f0d10
Mikel Bober-Irizar <mikel@mxbi.net>
2021-02-16 21:40:44 +0000

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,2 +1,2 @@
-gunicorn --certfile certs/fullchain.pem --keyfile certs/privkey.pem --bind 0.0.0.0:7355 app:app
-
+#gunicorn --certfile certs/fullchain.pem --keyfile certs/privkey.pem --bind 0.0.0.0:7355 app:app
+gunicorn --bind unix:/tmp/popskill-api.sock --umask 777 app:app
