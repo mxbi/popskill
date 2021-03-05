ed5c0eda3cf48a727ffd103e88d05bad84829f94
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-03-05 13:38:22 +0000

    Use steam.steamid to get user's steam64_id from their profile link (#5)
    * Use steam.steamid to get user's steam64_id from their profile link
    
    * Cast steam64_id to int

--- popflash_api.py
+++ popflash_api.py
@@ -34,9 +34,9 @@ def get_profile(url):
   df = pd.read_html(str(tab), header=0)[0]
   df['match_link'] = _strip_links_from_table(tab)
 
-  steam = soup.select('#page-container > div:nth-child(2) > div > div:nth-child(1) > h3 > span.steam-profile > a')[0]['href']
+  steam_profile = soup.select('#page-container > div:nth-child(2) > div > div:nth-child(1) > h3 > span.steam-profile > a')[0]['href']
 
-  return {'match_table': df, 'id': url.split('/')[-1], 'name': name, 'steam': steam, 'v': API_VERSION}
+  return {'match_table': df, 'id': url.split('/')[-1], 'name': name, 'steam_profile': steam_profile, 'v': API_VERSION}
 
 def get_match(url):
   if isinstance(url, int):
